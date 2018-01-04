ef7dfe81cf84f59822e650909e9d1c584e10cd72
ISS <Admin@sachin-mac-mini.local>
2018-01-04 15:51:23 +0530

    Initial Commit

---
+++ PermissionModel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PermissionModel
+//
+//  Created by ISS on 1/4/18.
+//  Copyright © 2018 ISS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
